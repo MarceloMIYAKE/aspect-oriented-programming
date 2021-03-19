972db4d570b506f99fae6c273861f9fe78aeb337
Marcelo Masahiko Miyake <mmiyake@atech.com.br>
2021-03-19 18:35:26 -0300

    feat(AOP): log de auditoria

--- src/main/java/com/github/marcelomiyake/aop/Calculadora.java
+++ src/main/java/com/github/marcelomiyake/aop/Calculadora.java
@@ -1,13 +1,24 @@
 package com.github.marcelomiyake.aop;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Calculadora {
 
+	private static Logger logger = LogManager.getLogger(Calculadora.class);
+
 	public static int adicao(final int a, final int b) {
-		return a + b;
+		logger.info("Entradas: {}, {}", a, b);
+		final int resultado = a + b;
+		logger.info("Saída: {}", resultado);
+		return resultado;
 	}
 
 	public static int subtracao(final int a, final int b) {
-		return a - b;
+		logger.info("Entradas: {}, {}", a, b);
+		final int resultado = a - b;
+		logger.info("Saída: {}", resultado);
+		return resultado;
 	}
 
 	private Calculadora() {
